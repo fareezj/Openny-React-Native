5b386cedc50b3ab68fd26bceb678e1d8791592a1
UF-Fareez <fareez.mohamad@hexainitiative.com>
2021-10-08 17:40:50 +0800

    Add months picker on analytic screen

--- app/screens/expense-analytics/analytics-screen.tsx
+++ app/screens/expense-analytics/analytics-screen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import { View, StyleSheet, ScrollView } from "react-native"
+import { View, StyleSheet, ScrollView, Image } from "react-native"
 import { Text, Screen } from "../../components"
 import { color, spacing } from "../../theme"
 import { LineChart, Grid, YAxis, XAxis } from "react-native-svg-charts"
@@ -7,28 +7,39 @@ import { useStores } from "../../models"
 import moment from "moment"
 import { HeaderBack } from "../../components/header-back/header-back"
 import { onSnapshot } from "mobx-state-tree"
+import { ExpenseItemData } from "../../utils/types"
+import { ExpenseDateSorter } from "../../utils/expenseDateSorter"
+import HorizontalPicker from "@vseslav/react-native-horizontal-picker"
 
 export const AnalyticScreen = () => {
   const { expenseStore } = useStores()
   const { expenses } = expenseStore
   const [dataMapping, setDataMapping] = useState<number[]>([50, 10, 40, 95, -4, 50, -20, -80])
   const [dataXValue, setDataXValue] = useState<number[]>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  const [currentMonth, setCurrentMonth] = useState<number>(1)
+  const [expenseExist, setExpenseExist] = useState<boolean>(false)
   const contentInset = { top: 20, bottom: 20 }
+  const Items = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  const itemWidth = 100
 
   useEffect(() => {
-    SortExpenseMapping()
-  }, [expenses])
+    const res = ExpenseDateSorter(expenses, currentMonth.toString())
+    SortExpenseMapping(res)
+  }, [expenses, currentMonth])
 
   onSnapshot(expenseStore, () => {
-    SortExpenseMapping()
+    const res = ExpenseDateSorter(expenses, currentMonth.toString())
+    SortExpenseMapping(res)
   })
 
-  function SortExpenseMapping() {
+  function SortExpenseMapping(expenseData: ExpenseItemData[]) {
     const finalXValue = []
     const finalYValue = []
 
+    expenseData.length > 0 ? setExpenseExist(true) : setExpenseExist(false)
+
     var holder = {}
-    expenses.forEach((val) => {
+    expenseData.forEach((val) => {
       if (holder.hasOwnProperty(val.date)) {
         holder[val.date] = parseFloat(holder[val.date]) + parseFloat(val.total)
       } else {
@@ -47,70 +58,88 @@ export const AnalyticScreen = () => {
         console.log("holder after", holder, holder[val])
       })
 
-    // const finalXValueSort = finalXValue
-    //   .reduce((prevVal, curVal) => {
-    //     if (parseInt(curVal.slice(0, 2)) > 0) {
-    //       prevVal.push(curVal.slice(0, 2))
-    //     }
-    //     return prevVal
-    //   }, [])
-    //   .sort((a, b) => {
-    //     return a - b
-    //   })
-    // console.log("x-value", finalXValueSort)
-    // console.log("y-value", finalYValue)
     setDataXValue(finalXValue)
     setDataMapping(finalYValue)
+  }
+
+  const renderMonth = (item) => (
+    <View style={{ width: itemWidth, alignItems: "center" }}>
+      <Text preset="fieldLabel" style={{ color: "white", fontSize: 22 }} text={item} />
+    </View>
+  )
+
+  const NoAnalyticRecorded = () => {
+    return (
+      <View style={AnalticsStyle.NO_EXPENSE_BASE}>
+        <Text
+          style={AnalticsStyle.NO_EXPENSE_TEXT}
+          text={`No expenses recorded in month: ${Items[currentMonth - 1]}`}
+        />
+      </View>
+    )
   }
 
   return (
     <View style={AnalticsStyle.FULL}>
       <Screen style={AnalticsStyle.CONTAINER} preset="fixed" backgroundColor={color.transparent}>
         <HeaderBack title={"Expense Analysis Graph"} />
-        <View style={{ borderWidth: 0, height: 350, marginTop: 50 }}>
-          <ScrollView horizontal={true} bounces={true}>
-            <View>
-              <Text text="RM" style={{ position: "absolute", left: 30 }} />
-            </View>
-            <View style={{ height: 300, flexDirection: "row", width: 600, marginTop: 20 }}>
-              <YAxis
-                data={dataMapping}
-                contentInset={contentInset}
-                svg={{
-                  fill: "white",
-                  fontSize: 12,
-                }}
-                numberOfTicks={5}
-                formatLabel={(value) => `${value.toFixed(2)}`}
-              />
-              <LineChart
-                style={{ height: 300, width: 500 }}
-                data={dataMapping}
-                svg={{ stroke: "rgb(134, 65, 244)" }}
-                contentInset={{ ...contentInset, left: 20, right: 20 }}
-              >
-                <Grid />
-              </LineChart>
-              {console.log(dataXValue)}
-              <XAxis
-                style={{
-                  marginHorizontal: -500,
-                  marginVertical: 300,
-                  height: 300,
-                  width: 500,
-                }}
-                data={dataXValue}
-                formatLabel={(value, index) =>
-                  moment(dataXValue[index], "DD MMM YYYY").format("DD")
-                }
-                contentInset={{ left: 20, right: 20 }}
-                svg={{ fontSize: 12, fill: "white" }}
-              />
+        <View style={{ borderWidth: 0, height: 450, marginTop: 50 }}>
+          <View style={{ height: 40 }}>
+            <HorizontalPicker
+              data={Items}
+              renderItem={renderMonth}
+              itemWidth={itemWidth}
+              onChange={(val) => setCurrentMonth(parseInt(moment(Items[val], "MMM").format("MM")))}
+            />
+          </View>
+          <View style={{ justifyContent: "center", flexDirection: "row" }}>
+            <Image
+              source={require("../../../assets/up-arrows.png")}
+              style={{ width: 30, height: 30, tintColor: "white" }}
+            />
+          </View>
+          {expenseExist ? (
+            <ScrollView horizontal={true} bounces={true} style={{ paddingTop: 20 }}>
               <View>
-                <Text text="Days" style={{ position: "absolute", top: 300, left: 520 }} />
+                <Text text="RM" style={AnalticsStyle.Y_LABEL} />
               </View>
-            </View>
-          </ScrollView>
+              <View style={AnalticsStyle.CHART_BASE}>
+                <YAxis
+                  data={dataMapping}
+                  contentInset={contentInset}
+                  svg={{
+                    fill: "white",
+                    fontSize: 12,
+                  }}
+                  numberOfTicks={5}
+                  formatLabel={(value) => `${value.toFixed(2)}`}
+                />
+                <LineChart
+                  style={{ height: 300, width: 500 }}
+                  data={dataMapping}
+                  svg={{ stroke: "rgb(134, 65, 244)" }}
+                  contentInset={{ ...contentInset, left: 20, right: 20 }}
+                >
+                  <Grid />
+                </LineChart>
+                {console.log(dataXValue)}
+                <XAxis
+                  style={AnalticsStyle.X_AXIS}
+                  data={dataXValue}
+                  formatLabel={(value, index) =>
+                    moment(dataXValue[index], "DD MMM YYYY").format("DD")
+                  }
+                  contentInset={{ left: 20, right: 20 }}
+                  svg={{ fontSize: 12, fill: "white" }}
+                />
+                <View>
+                  <Text text="Days" style={AnalticsStyle.X_LABEL} />
+                </View>
+              </View>
+            </ScrollView>
+          ) : (
+            <NoAnalyticRecorded />
+          )}
         </View>
       </Screen>
     </View>
@@ -118,6 +147,41 @@ export const AnalyticScreen = () => {
 }
 
 const AnalticsStyle = StyleSheet.create({
+  NO_EXPENSE_BASE: {
+    backgroundColor: "white",
+    borderRadius: 30,
+    height: 50,
+    width: 330,
+    marginTop: 80,
+    alignSelf: "center",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  NO_EXPENSE_TEXT: {
+    color: "black",
+    textAlign: "center",
+  },
+  Y_LABEL: {
+    position: "absolute",
+    left: 30,
+  },
+  CHART_BASE: {
+    height: 300,
+    flexDirection: "row",
+    width: 600,
+    marginTop: 20,
+  },
+  X_LABEL: {
+    position: "absolute",
+    top: 300,
+    left: 520,
+  },
+  X_AXIS: {
+    marginHorizontal: -500,
+    marginVertical: 300,
+    height: 300,
+    width: 500,
+  },
   FULL: { flex: 1, backgroundColor: "#0E164C" },
   TITLE: {
     fontSize: 22,
